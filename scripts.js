33ed9046939d7cee599a7a5d72a8d70931334b00
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-17 09:13:07 +0200

    Added a function to create and append book previews

--- scripts.js
+++ scripts.js
@@ -7,27 +7,25 @@ let matches = books;
 // Function to get DOM elements
 const getElement = (selector) => document.querySelector(selector);
 
-//Loop through initial books and create HTML  elements
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-  const element = document.createElement("button");
-  element.classList = "preview";
-  element.setAttribute("data-preview", id);
-
-  //Construct HTML content for each book
-  element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
+// Function to create and append book previews
+const createBookPreviews = (books, container) => {
+  const fragment = document.createDocumentFragment();
+  books.forEach(({ author, id, image, title }) => {
+    const element = document.createElement("button");
+    element.classList = "preview";
+    element.setAttribute("data-preview", id);
+    element.innerHTML = `
+      <img class="preview__image" src="${image}" />
+      <div class="preview__info">
+        <h3 class="preview__title">${title}</h3>
+        <div class="preview__author">${authors[author]}</div>
+      </div>
     `;
+    fragment.appendChild(element);
+  });
+  container.appendChild(fragment);
+};
 
-  starting.appendChild(element);
-}
 
 //Append initial book elements to the DOM
 document.querySelector("[data-list-items]").appendChild(starting);
