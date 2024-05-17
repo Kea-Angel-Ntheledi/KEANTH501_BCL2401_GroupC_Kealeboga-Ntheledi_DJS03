09faf7a0a50f5605f9dd66ae4fed4cb0107443f7
Kea-Angel-Ntheledi <kealebogaangelntheledi@gmail.com>
2024-05-17 09:23:42 +0200

    Added more event listeners

--- scripts.js
+++ scripts.js
@@ -97,46 +97,78 @@ const applySearchFilters = (filters) => {
 };
 
 
-// Handle cancel button clicks
-function handleCancelClick(cancelSelector, overlaySelector) {
-  document.querySelector(cancelSelector).addEventListener("click", () => {
-    document.querySelector(overlaySelector).open = false;
-  });
-}
+// Event listeners
+getElement("[data-search-cancel]").addEventListener("click", () =>
+  closeOverlay("[data-search-overlay]")
+);
+getElement("[data-settings-cancel]").addEventListener("click", () =>
+  closeOverlay("[data-settings-overlay]")
+);
+getElement("[data-header-search]").addEventListener("click", () =>
+  openOverlay("[data-search-overlay]", "[data-search-title]")
+);
+getElement("[data-header-settings]").addEventListener("click", () =>
+  openOverlay("[data-settings-overlay]")
+);
+getElement("[data-list-close]").addEventListener("click", () =>
+  closeOverlay("[data-list-active]")
+);
 
-handleCancelClick("[data-search-cancel]", "[data-search-overlay]");
-handleCancelClick("[data-settings-cancel]", "[data-settings-overlay]");
-handleCancelClick("[data-list-close]", "[data-list-active]");
-
-document.querySelector("[data-header-search]").addEventListener("click", () => {
-  document.querySelector("[data-search-overlay]").open = true;
-  document.querySelector("[data-search-title]").focus();
-});
-
-document.querySelector("[data-header-settings]").addEventListener("click", () => {
-  document.querySelector("[data-settings-overlay]").open = true;
-});
-
-// Add click event listener to the "save" button
-document.querySelector("[name='save']").addEventListener("click", (event) => {
-  // Prevent the form from being submitted
+getElement("[data-settings-form]").addEventListener("submit", (event) => {
   event.preventDefault();
-
-  // Get the theme value from the form
-  const theme = document.querySelector('[name="theme"]').value;
-
-  // Set theme based on user selection
-  if (theme === "night") {
-    document.documentElement.style.setProperty("--color-dark", "255, 255, 255");
-    document.documentElement.style.setProperty("--color-light", "10, 10, 20");
-  } else {
-    document.documentElement.style.setProperty("--color-dark", "10, 10, 20");
-    document.documentElement.style.setProperty("--color-light", "255, 255, 255");
-  }
-
-  // Optionally, close the settings overlay after saving
-  document.querySelector("[data-settings-overlay]").open = false;
+  const formData = new FormData(event.target);
+  const { theme } = Object.fromEntries(formData);
+  applyTheme(theme);
+  closeOverlay("[data-settings-overlay]");
 });
+
+getElement("[data-search-form]").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const filters = Object.fromEntries(formData);
+  matches = applySearchFilters(filters);
+  page = 1;
+  getElement("[data-list-message]").classList.toggle(
+    "list__message_show",
+    matches.length < 1
+  );
+  getElement("[data-list-items]").innerHTML = "";
+  createBookPreviews(
+    matches.slice(0, BOOKS_PER_PAGE),
+    getElement("[data-list-items]")
+  );
+  updateShowMoreButton();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+  closeOverlay("[data-search-overlay]");
+});
+
+getElement("[data-list-button]").addEventListener("click", () => {
+  createBookPreviews(
+    matches.slice(page * BOOKS_PER_PAGE, (page + 1) * BOOKS_PER_PAGE),
+    getElement("[data-list-items]")
+  );
+  page += 1;
+  updateShowMoreButton();
+});
+
+getElement("[data-list-items]").addEventListener("click", (event) => {
+  const pathArray = Array.from(event.composedPath());
+  const active = pathArray.find((node) => node?.dataset?.preview);
+  if (active) {
+    const book = books.find((book) => book.id === active.dataset.preview);
+    if (book) {
+      getElement("[data-list-active]").open = true;
+      getElement("[data-list-blur]").src = book.image;
+      getElement("[data-list-image]").src = book.image;
+      getElement("[data-list-title]").innerText = book.title;
+      getElement("[data-list-subtitle]").innerText = `${
+        authors[book.author]
+      } (${new Date(book.published).getFullYear()})`;
+      getElement("[data-list-description]").innerText = book.description;
+    }
+  }
+});
+
 
 
     // Close settings overlay
